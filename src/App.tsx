f779cb9f20cd18352b37f1193a92172b0451ec35
Eniitan <enitanolawale0@gmail.com>
2026-05-31 21:37:26 +0100

    first look

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import Logo from "./assets/logo2transp.png";
+import BackgroundImage from "./assets/BG_image_1.jpg";
+
 function App() {
   return (
-    <main>
-      <p className="bg-black">Hello world</p>
+    <main
+      className="w-screen h-screen flex items-center justify-center bg-[55%_60%] bg-no-repeat"
+      style={{ backgroundImage: `url(${BackgroundImage})` }}>
+      <div>
+        <img
+          src={Logo}
+          alt=""
+          width={700}
+        />
+        <p className="absolute top-155 z-50 left-140 text-white font-extrabold text-4xl">
+          COMING SOON!!!
+        </p>
+      </div>
     </main>
   );
 }
