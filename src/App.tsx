feb16dab2c6fa2452533a2241454bea07e45e9cb
Eniitan <enitanolawale0@gmail.com>
2026-06-02 21:00:01 +0100

    marquee stable on desktop

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,19 @@ import BackgroundImage from "./assets/BG_image_1.jpg";
 function App() {
   return (
     <main
-      className="w-screen h-screen flex items-center justify-center bg-[55%_60%] bg-no-repeat"
+      className="w-screen h-screen relative flex items-center justify-center bg-[55%_60%] bg-no-repeat overflow-hidden"
       style={{ backgroundImage: `url(${BackgroundImage})` }}>
-      <div>
+      <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
         <img
           src={Logo}
           id="Logo"
           alt=""
           width={700}
         />
-        <p className="absolute top-155 z-50 left-140 text-white font-extrabold text-4xl">
-          COMING SOON!!!
+      </div>
+      <div className="absolute left-0 bottom-16 w-screen overflow-hidden z-50">
+        <p className="whitespace-nowrap tracking-wide text-white font-extrabold text-4xl">
+          R1 WE RUN D STREETS, COMING SOON!!!, WATCH THIS SPACE
         </p>
       </div>
     </main>
